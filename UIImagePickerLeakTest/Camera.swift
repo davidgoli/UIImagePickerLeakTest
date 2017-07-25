24d4d11342ad581c6a029f1a22cc3c0a8c75a0ec
davidgoli <davigoli@gmail.com>
2017-07-25 15:42:18 -0700

    even more minimal repro

--- UIImagePickerLeakTest/Camera.swift
+++ UIImagePickerLeakTest/Camera.swift
@@ -6,57 +6,26 @@ protocol Camera {
 }
 
 class CameraReal: NSObject, Camera, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    @IBOutlet var overlayView: UIView!
-    @IBOutlet weak var imagesCount: UILabel!
-
     weak var viewController: UIViewController!
-    var imagePicker: UIImagePickerController!
-    var pictureTaken: ((UIImage) -> Bool)!
 
     func takePictures(from viewController: UIViewController) {
         guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
             return
         }
 
-        self.viewController = viewController
+        let imagePicker = createImagePicker()
 
-        createImagePicker()
-        setupOverlay()
-
-        viewController.present(imagePicker!, animated: true, completion: nil)
-    }
-
-    // MARK: - IBActions
-
-    @IBAction func doneTapped(_ sender: Any) {
-        viewController?.dismiss(animated: true)
-    }
-
-    @IBAction func captureTapped(_ sender: Any) {
-        self.imagePicker?.takePicture()
-    }
-
-    // MARK: - UIImagePickerControllerDelegate
-
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        viewController.present(imagePicker, animated: true, completion: nil)
     }
 
     // MARK: - Private
 
-    func createImagePicker() {
+    func createImagePicker() -> UIImagePickerController {
         let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
         imagePicker.sourceType = .camera
         imagePicker.mediaTypes = [kUTTypeImage as String]
         imagePicker.allowsEditing = false
-        imagePicker.showsCameraControls = false
-        self.imagePicker = imagePicker
-    }
-
-    func setupOverlay() {
-        Bundle.main.loadNibNamed("CameraOverlay", owner: self, options: nil)
-        overlayView.frame = imagePicker!.cameraOverlayView!.frame
-        imagePicker!.cameraOverlayView = overlayView
-        overlayView = nil
+        imagePicker.showsCameraControls = true
+        return imagePicker
     }
 }
